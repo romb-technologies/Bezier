4e9f5038907cb5b639ac982b0fbbacf9f48dc44f
stribor14 <mirkokot@gmail.com>
2019-08-19 12:21:55 +0200

    Add declarations descriptions

--- BezierCpp/declarations.h
+++ BezierCpp/declarations.h
@@ -7,9 +7,37 @@
 #include <memory>
 #include <deque>
 
+/*!
+ * Nominal namespace containing class pre-definitions and typedefs
+ */
 namespace Bezier
 {
+/*!
+ * \brief A Bezier curve class
+ *
+ * A class for storing and using any-order Bezier curve.
+ * It uses private and static caching for storing often accessed data.
+ * Private caching is used for data concerning individual curve, while
+ * static caching is used for common data (coefficient matrices)
+ */
 class Curve;
+
+/*!
+ * \brief A polyline class
+ *
+ * A class for operations on polyline, mainly used for
+ * polyline representation of curves.
+ */
+class PolyLine;
+
+/*!
+ * \brief A Bezier polycurve class
+ *
+ * A class for linking multiple Bezier curves with at least
+ * C0 continuity. It allows subcurve and continuity manipulation.
+ * Both parametric and geometric continuity are supported.
+ */
+class PolyCurve;
 
 /*!
  * \brief Shared pointer of Curve
@@ -35,11 +63,13 @@ typedef std::vector<Point> PointVector;
  * \brief Bounding box
  */
 typedef Eigen::AlignedBox2d BBox;
-
-class PolyLine;
-
-class PolyCurve;
+/*!
+ * \brief Shared pointer of Polycurve
+ */
 typedef std::shared_ptr<PolyCurve> PolyCurvePtr;
+/*!
+ * \brief Shared pointer of const Polycurve
+ */
 typedef std::shared_ptr<const PolyCurve> ConstPolyCurvePtr;
 }
 #endif // DECLARATIONS_H
