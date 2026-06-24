5169b4a64face42cc2580f2a9bdb2b4c324aeff4
stribor14 <mirko.kokot@romb-technologies.hr>
2026-06-24 11:27:56 +0200

    Add small dashboard with information

--- example/mainwindow.cpp
+++ example/mainwindow.cpp
@@ -1,7 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include <QDebug>
+#include <QFrame>
+#include <QLabel>
+#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow), scene(new CustomScene)
 {
@@ -9,6 +11,10 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
 
   ui->graphicsView->setScene(scene);
   new QGraphicsViewZoom(ui->graphicsView);
+
+  buildDashboard();
+  connect(scene, &QGraphicsScene::selectionChanged, this, &MainWindow::refreshDashboard);
+  connect(scene, &QGraphicsScene::changed, this, &MainWindow::refreshDashboard);
 
   Eigen::MatrixX2d cp1, cp2;
   cp1.resize(4, 2);
@@ -28,6 +34,59 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
   scene->addItem(new qCurve(cp2 * 5));
 
   ui->graphicsView->centerOn(scene->itemsBoundingRect().center());
+  refreshDashboard();
 }
 
 MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::buildDashboard()
+{
+  auto* panel = new QFrame;
+  panel->setFrameShape(QFrame::StyledPanel);
+  panel->setMinimumWidth(200);
+  panel->setMaximumWidth(240);
+
+  auto* vbox = new QVBoxLayout(panel);
+  vbox->addWidget(lbl_curves_ = new QLabel);
+  vbox->addWidget(lbl_polycurves_ = new QLabel);
+  vbox->addWidget(lbl_selected_ = new QLabel);
+  vbox->addWidget(lbl_order_ = new QLabel);
+  vbox->addWidget(lbl_length_ = new QLabel);
+  vbox->addWidget(lbl_offset_ = new QLabel);
+  vbox->addStretch(1);
+  // Buttons (draw mode, control-point mode, help) will be added here.
+
+  ui->gridLayout->addWidget(panel, 0, 1);
+  ui->gridLayout->setColumnStretch(0, 1);
+}
+
+void MainWindow::refreshDashboard()
+{
+  int n_curves = 0, n_polycurves = 0;
+  for (auto* item : scene->items())
+  {
+    if (item->type() == QGraphicsItem::UserType + 1)
+      n_curves++;
+    else if (item->type() == QGraphicsItem::UserType + 2)
+      n_polycurves++;
+  }
+
+  auto selected = scene->selectedItems();
+  lbl_curves_->setText(QString("Curves: %1").arg(n_curves));
+  lbl_polycurves_->setText(QString("Polycurves: %1").arg(n_polycurves));
+  lbl_selected_->setText(QString("Selected: %1").arg(selected.size()));
+
+  if (selected.size() == 1 && selected.first()->type() == QGraphicsItem::UserType + 1)
+  {
+    auto* curve = static_cast<qCurve*>(selected.first());
+    lbl_order_->setText(QString("Order: %1").arg(curve->order()));
+    lbl_length_->setText(QString("Length: %1").arg(curve->length(), 0, 'f', 1));
+  }
+  else
+  {
+    lbl_order_->setText("Order: –");
+    lbl_length_->setText("Length: –");
+  }
+
+  lbl_offset_->setText(QString("Offset: %1").arg(scene->offset(), 0, 'f', 0));
+}
--- example/mainwindow.h
+++ example/mainwindow.h
@@ -5,6 +5,8 @@
 
 #include "customscene.h"
 
+class QLabel;
+
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
@@ -14,8 +16,18 @@ public:
   ~MainWindow();
 
 private:
+  void buildDashboard();
+  void refreshDashboard();
+
   Ui::MainWindow* ui;
   CustomScene* scene;
+
+  QLabel* lbl_curves_;
+  QLabel* lbl_polycurves_;
+  QLabel* lbl_selected_;
+  QLabel* lbl_order_;
+  QLabel* lbl_length_;
+  QLabel* lbl_offset_;
 };
 
 #endif // MAINWINDOW_H
